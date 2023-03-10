43d179c37993f411f983b48afbe2631f27ce5f93
pjgill010 <pjgill010@gmail.com>
2023-03-09 22:26:19 -0800

    final results

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -45,7 +45,6 @@ ON dept_emp.dept_no = departments.dept_no
 WHERE departments.dept_name = 'Sales';
 
 
-
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
